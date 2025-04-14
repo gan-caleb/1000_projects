9d278a18052e73e57036af281ebbdc1e75767d58
Caleb Gan <gan-jia-le.caleb.p@sew-eurodrive.de>
2025-04-14 11:25:09 +0200

    compile working but average function is wrong

--- cpp/2_grade-calc/gradecalc.cpp
+++ cpp/2_grade-calc/gradecalc.cpp
@@ -1,8 +1,22 @@
 #include "gradecalc.h"
 #include <numeric>
+#include <algorithm>
 
-double calculateAverage(const vector<int> grades) {
+double calculateAverage(const vector<int>& grades) {
     // Use accumulate function (start, end, init)
-    double average = accumulate(grades.begin(), grades.end(), 0);
-    return average;
+    double averageGrade = accumulate(grades.begin(), grades.end(), 0);
+    return averageGrade;
 }
+
+int findMax(const vector<int>& grades) {
+    // Use max_element (range start and end)
+    int maxGrade = *max_element(grades.begin(), grades.end());
+    return maxGrade;
+}
+
+int findMin(const vector<int>& grades) {
+    // Use min_element
+    int minGrade = *min_element(grades.begin(), grades.end());
+    return minGrade;
+}
+
--- cpp/2_grade-calc/main.cpp
+++ cpp/2_grade-calc/main.cpp
@@ -5,6 +5,15 @@ using namespace std;
 
 int main() {
 
+/*
+### Pseudocode ###
+
+Ask for user input and store in list
+Then, perform each function on list
+Print results
+
+*/
+
     // Define GLOBAL variables
     vector<int> grades;
     int input;
@@ -15,11 +24,20 @@ int main() {
         cout << "Input each grade 1 by 1 and press ENTER: ";
         cin >> input;
         // store user input into array
-        grades.push_back(input);
-        
+        grades.push_back(input); 
         if (input == -1) {
             finishedInput = true;
         }
+    }
+
+    if (finishedInput) {
+        double average = calculateAverage(grades);
+        int max = findMax(grades);
+        int min = findMin(grades);
+
+        cout << "Average grade: " << average << "\n";
+        cout << "Highest grade: " << max << "\n";
+        cout << "Lowest grade: " << min << "\n";
     }
 
     return 0;
